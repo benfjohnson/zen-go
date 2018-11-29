46c628c31ed014fde654ca15b316f93b36174f6c
Ben Johnson <ben.johnson@avalara.com>
2018-11-28 21:47:44 -0800

    add package.json, install elm and elm-format as dev dependencies

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -5,6 +5,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
+
 main : Program () Model Msg
 main =
     Browser.sandbox
@@ -13,15 +14,23 @@ main =
         , update = update
         }
 
+
+
 -- MODEL
 
-type Stone =
-    Black
+
+type Stone
+    = Black
     | White
 
-type alias CurrentTurn = Stone
 
-type alias Board = List (List (Maybe Stone))
+type alias CurrentTurn =
+    Stone
+
+
+type alias Board =
+    List (List (Maybe Stone))
+
 
 type alias Model =
     { board : Board
@@ -29,11 +38,14 @@ type alias Model =
     }
 
 
+
 -- VIEW
+
 
 boardRow : List (Maybe Stone)
 boardRow =
     [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+
 
 init : Model
 init =
@@ -41,73 +53,116 @@ init =
     , currentTurn = Black
     }
 
-boardPositionView : (Int, Int) -> Maybe Stone -> Html Msg
+
+boardPositionView : ( Int, Int ) -> Maybe Stone -> Html Msg
 boardPositionView coordinates maybeGp =
     case maybeGp of
         Nothing ->
             div [ class "board-pos empty-pos", onClick (Move coordinates) ] []
+
         Just gp ->
             case gp of
                 Black ->
                     div [ class "board-pos occupied-pos black-pos" ] []
+
                 White ->
                     div [ class "board-pos occupied-pos white-pos" ] []
 
+
 boardRowView : Int -> List (Maybe Stone) -> Html Msg
 boardRowView ypos row =
-    div [ class "board-row" ] (List.indexedMap (\ xpos stone -> (boardPositionView (xpos, ypos) stone)) row)
+    div [ class "board-row" ] (List.indexedMap (\xpos stone -> boardPositionView ( xpos, ypos ) stone) row)
+
 
 boardView : Board -> Html Msg
 boardView board =
     div [ class "game-board" ] (List.indexedMap boardRowView board)
 
+
 view : Model -> Html Msg
 view model =
-    div [] [
-        h1 [] [ text "zen go!" ]
+    div []
+        [ h1 [] [ text "zen go!" ]
         , h2 [] [ (model.currentTurn |> Debug.toString) ++ "'s turn to play" |> text ]
         , boardView model.board
-    ]
+        ]
+
 
 
 -- UPDATE
 
-type Msg =
-    NoOp
-    | Move (Int, Int)
+
+type Msg
+    = NoOp
+    | Move ( Int, Int )
+
+
 
 -- As this project develops, this function will contain all logic used to assess the validity of an attempted move
 -- For now, only prevent a placed stone if there's already one there!
+
+
 isStoneLegal : Stone -> Maybe Stone -> Bool
 isStoneLegal stone currentPoint =
     case currentPoint of
-        Nothing -> True
-        Just _ -> False
+        Nothing ->
+            True
+
+        Just _ ->
+            False
 
 
-playStone : Stone -> (Int, Int) -> Board -> Board
-playStone stone (xpos, ypos) board =
+playStone : Stone -> ( Int, Int ) -> Board -> Board
+playStone stone ( xpos, ypos ) board =
     let
         placeStone currentPoint =
-            if (isStoneLegal stone currentPoint) then Just stone else currentPoint
+            if isStoneLegal stone currentPoint then
+                Just stone
+
+            else
+                currentPoint
+
         newRow row =
-            List.indexedMap (\pointIndex currPoint -> if pointIndex == xpos then (placeStone currPoint) else currPoint) row
+            List.indexedMap
+                (\pointIndex currPoint ->
+                    if pointIndex == xpos then
+                        placeStone currPoint
+
+                    else
+                        currPoint
+                )
+                row
+
         newBoard =
-            List.indexedMap (\rowIndex currRow -> if rowIndex == ypos then (newRow currRow) else currRow) board
+            List.indexedMap
+                (\rowIndex currRow ->
+                    if rowIndex == ypos then
+                        newRow currRow
+
+                    else
+                        currRow
+                )
+                board
     in
-        newBoard
+    newBoard
 
 
-update: Msg -> Model -> Model
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Move coords ->
             case model.currentTurn of
                 Black ->
-                    { model | board = playStone Black coords model.board
-                    , currentTurn = White }
-                White ->
-                    { model | board = playStone White coords model.board
-                    , currentTurn = Black }
-        default -> model
+                    { model
+                        | board = playStone Black coords model.board
+                        , currentTurn = White
+                    }
 
+                White ->
+                    { model
+                        | board = playStone White coords model.board
+                        , currentTurn = Black
+                    }
+
+        default ->
+            model
